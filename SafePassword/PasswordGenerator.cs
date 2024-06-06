6a226915afbb9082850e9597e4ec76b806644446
FedericoG2 <fg0170700@gmail.com>
2024-06-05 21:50:47 -0300

    Agregar archivos de proyecto.

---
+++ SafePassword/PasswordGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePassword
+{
+    internal class PasswordGenerator
+    {
+        private static readonly string DefaultChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        private static readonly string SpecialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+        public static string GeneratePassword(int length, bool includeSpecialChars = true, bool includeNumbers = true)
+        {
+            StringBuilder charSet = new StringBuilder(DefaultChars);
+
+            if (includeSpecialChars)
+            {
+                charSet.Append(SpecialChars);
+            }
+
+            if (!includeNumbers)
+            {
+                charSet = charSet.Replace("0123456789", "");
+            }
+
+            return GeneratePasswordFromCharSet(charSet.ToString(), length);
+        }
+
+        private static string GeneratePasswordFromCharSet(string charSet, int length)
+        {
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(charSet.Length);
+                password.Append(charSet[index]);
+            }
+
+            return password.ToString();
+        }
+    }
+}
---
+++ SafePassword/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generador de Contraseñas Seguras");
+
+            Console.Write("Ingrese la longitud de la contraseña: ");
+            int length;
+            while (!int.TryParse(Console.ReadLine(), out length) || length <= 0)
+            {
+                Console.WriteLine("Por favor, ingrese un número entero positivo.");
+                Console.Write("Ingrese la longitud de la contraseña: ");
+            }
+
+            Console.Write("¿Incluir caracteres especiales? (s/n): ");
+            bool includeSpecialChars = Console.ReadLine().ToLower() == "s";
+
+            Console.Write("¿Incluir números? (s/n): ");
+            bool includeNumbers = Console.ReadLine().ToLower() == "s";
+
+            string password = PasswordGenerator.GeneratePassword(length, includeSpecialChars, includeNumbers);
+
+            Console.WriteLine($"Contraseña generada: {password}");
+
+            Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
